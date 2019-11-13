31180ea41cc777f4fabd91e74d6a0298ff8ddf63
Alexey SongVo <52455378+songvo1018@users.noreply.github.com>
2019-11-13 10:38:24 +0700

    Ver 1.0
    Now there is a page that contains three gradient display modes. Modes differ in different speeds, color schemes and direction.
    
    In the future, you can add a choice of colors from the color picker, but ease of use may be lost. add a mode to start all modes one by one. Add new modes.

---
+++ script.js
@@ -0,0 +1,26 @@
+function bckg1() {
+    document.getElementById("bckg").classList.toggle('background1');
+    document.getElementById("activate1").classList.toggle('selected');
+    
+}
+
+document.getElementById('activate1').addEventListener('click', bckg1);
+
+function bckg2() {
+    document.getElementById("bckg").classList.toggle('background2');
+    document.getElementById("activate2").classList.toggle('selected');
+    
+
+}
+
+document.getElementById('activate2').addEventListener('click', bckg2);
+
+
+
+function bckg3() {
+    document.getElementById("bckg").classList.toggle('background3');
+    document.getElementById("activate3").classList.toggle('selected');
+
+}
+
+document.getElementById('activate3').addEventListener('click', bckg3);
